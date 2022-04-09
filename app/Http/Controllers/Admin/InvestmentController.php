d0b59d967b27912ea10a7a963ef9bdc61ed7296c
devProMaleek <milikiadbay@gmail.com>
2022-04-09 15:03:10 +0100

    refactor: Changes to the admin side

--- app/Http/Controllers/Admin/InvestmentController.php
+++ app/Http/Controllers/Admin/InvestmentController.php
@@ -179,7 +179,7 @@ class InvestmentController extends Controller
             $datum['investment_date'] = '<span class="text-gray-600 fw-bolder d-block fs-6" style="white-space: nowrap;">'.$investment->investment_date->format('M d, Y').'</span>';
             $datum['status'] = $status;
             $datum['payment'] = $payment;
-            $datum['action'] = '<a href="javascript:void();" class="btn btn-sm btn-light-primary btn-active-primary" data-kt-menu-trigger="click" style="white-space: nowrap" data-kt-menu-placement="bottom-end" style="white-space: nowrap;">Action
+            $datum['action'] = '<a href="javascript:void();" class="btn btn-sm btn-primary btn-active-primary" data-kt-menu-trigger="click" style="white-space: nowrap" data-kt-menu-placement="bottom-end" style="white-space: nowrap;">Action
                                     <span class="svg-icon svg-icon-5 m-0">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                                             <path d="M11.4343 12.7344L7.25 8.55005C6.83579 8.13583 6.16421 8.13584 5.75 8.55005C5.33579 8.96426 5.33579 9.63583 5.75 10.05L11.2929 15.5929C11.6834 15.9835 12.3166 15.9835 12.7071 15.5929L18.25 10.05C18.6642 9.63584 18.6642 8.96426 18.25 8.55005C17.8358 8.13584 17.1642 8.13584 16.75 8.55005L12.5657 12.7344C12.2533 13.0468 11.7467 13.0468 11.4343 12.7344Z" fill="black" />
