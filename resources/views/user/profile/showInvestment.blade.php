7b76c76239822226ba3d0ad1bc6fbbb1b4ad3d71
Rafiu Moshood <meezyloaded@gmail.com>
2022-06-17 19:20:38 +0100

    fix: increase precision

--- resources/views/user/profile/showInvestment.blade.php
+++ resources/views/user/profile/showInvestment.blade.php
@@ -220,10 +220,10 @@
                         <!--end::Stats-->
                         <!--begin::Progress-->
                         @php
-                            $total = Illuminate\Support\Carbon::parse($investment['start_date'])->diffInDays($investment['return_date']);
-                            $passed = $total - now()->diffInDays($investment['return_date']);
+                            $total = Illuminate\Support\Carbon::parse($investment['start_date'])->diffInSeconds($investment['return_date']);
+                            $passed = $total - now()->diffInSeconds($investment['return_date']);
                             if ($investment['status'] == 'active') {
-                                $percentage = round(($passed / $total) * 100);
+                                $percentage = ($passed / $total) * 100;
                             } elseif ($investment['status'] == 'settled') {
                                 $percentage = 100;
                             } else {
@@ -246,7 +246,7 @@
                                 @else
                                     {{ ucwords($investment['status']) }}
                                 @endif</span>
-                                <span class="fw-bolder fs-6">{{$percentage}}%</span>
+                                <span class="fw-bolder fs-6">{{round($percentage)}}%</span>
                             </div>
                             <div class="h-30px mx-3 w-100 bg-light mb-3" style="border-radius: 30px;">
                                 <div class="bg-success h-30px" role="progressbar" style="border-radius: 30px; width: {{ $percentage }}%;" aria-valuenow="{{ $percentage }}" aria-valuemin="0" aria-valuemax="100"></div>
