e03f90a2253f694e4e7cc0c5f2ec8eae8f361cca
Sanni David <sannidavidsmart@gmail.com.com>
2022-10-11 16:21:28 +0100

    add international bank for users

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/pdf', function(){
-//     return view('pdf.certificate');
-// });
+Route::get('/pdf', function(){
+    return view('pdf.certificate');
+});
 
 Route::get('/', [App\Http\Controllers\StaticPageController::class, 'home'])->name('static.home');
 Route::get('/about', [App\Http\Controllers\StaticPageController::class, 'about'])->name('static.about');
@@ -48,6 +48,7 @@ Route::group(['middleware' => ['auth', 'active_user', 'verified', 'two_factor']]
 
 
     Route::post('/banks', [App\Http\Controllers\BankAccountsController::class, 'store'])->name('bank.store');
+    Route::post('/international/banks', [App\Http\Controllers\BankAccountsController::class, 'storeInt'])->name('internationalBank.store');
     Route::delete('/banks/{bank}', [App\Http\Controllers\BankAccountsController::class, 'destroy'])->name('bank.destroy');
 
     Route::post('/documents', [App\Http\Controllers\DocumentController::class, 'store'])->name('document.store');
