4cfb072d739967ffd95113ee950670fa3f8a3539
GokeAdewuyi <adewuyiyusuf@yahoo.com>
2022-12-09 08:46:15 +0100

    feat: add pagination helper

--- app/Helpers/Pagination.php
+++ app/Helpers/Pagination.php
@@ -1,1 +1,15 @@
 <?php
+
+if (!function_exists('get_page')) {
+    function get_page(): int
+    {
+        return (int) (request()->input('page') ?? 1);
+    }
+}
+
+if (!function_exists('get_per_page')) {
+    function get_per_page(): int
+    {
+        return (int) (request()->input('per_page') ?? 50);
+    }
+}
