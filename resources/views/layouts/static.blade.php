c9a60797c9692a27e178a4456c176386902be411
GokeAdewuyi <adewuyiyusuf@yahoo.com>
2023-07-06 16:55:44 +0100

    fix: tawk to

--- resources/views/layouts/static.blade.php
+++ resources/views/layouts/static.blade.php
@@ -342,14 +342,14 @@
 @yield('scripts')
 <!--Start of Tawk.to Script-->
 <script type="text/javascript">
-    var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-    (function () {
-        var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-        s1.async = true;
-        s1.src = 'https://embed.tawk.to/6304e16a54f06e12d89047c6/1gb5h12rt';
-        s1.charset = 'UTF-8';
-        s1.setAttribute('crossorigin', '*');
-        s0.parentNode.insertBefore(s1, s0);
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/6497368acc26a871b024799a/default';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
     })();
 </script>
 <!--End of Tawk.to Script-->
